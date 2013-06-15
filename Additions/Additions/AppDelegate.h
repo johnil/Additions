f46d537a40bd449dcc4f5a0deb3755e898beadc3
Johnil <johnil@me.com>
2013-06-15 18:58:42 +0800

    修复编辑bug

---
+++ Additions/Additions/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Additions
+//
+//  Created by Johnil on 13-6-15.
+//  Copyright (c) 2013年 Johnil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
--- Additions/Additions/main.m
+++ Additions/Additions/main.m
@@ -8,8 +8,11 @@
 
 #import <UIKit/UIKit.h>
 
-//#import "AppDelegate.h"
+#import "AppDelegate.h"
 
 int main(int argc, char *argv[])
 {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
 }
